c1dce6a080e6bd6e1ea8989085bb5adc5ae94a90
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2024-06-23 14:16:08 -0400

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,32 +60,26 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Abhi Challa.</h2>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a student studying Computer Science and Statistics at Rutgers University - New Brunswick. 
+        
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.linkedin.com/in/abhinav-challa-10a339216/"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my LinkedIn!
     </a>
   );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, four, five];
 
   return (
     <StyledHeroSection>
