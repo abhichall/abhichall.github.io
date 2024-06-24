e2b1458d8c54b1e31c93c78e208b8af894e30bee
Abhi Challa <158411574+abhichall@users.noreply.github.com>
2024-06-24 17:10:46 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -108,18 +108,7 @@ const Footer = () => {
         <a href="">
           <div>Inspired by Brittany Chiang</div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
+         
         </a>
       </StyledCredit>
     </StyledFooter>
